5778010e700b2ce4548f842d9978428eee60e0e1
75A3046F337F70A43B766F6D201E11FB <18612183801@163.com>
2020-07-16 14:26:29 +0800

    前25分
    Signed-off-by: 75A3046F337F70A43B766F6D201E11FB <18612183801@163.com>

--- seleniums/open.py
+++ seleniums/open.py
@@ -6,6 +6,7 @@
 # @File : open.py
 # @Software: PyCharm
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -40,32 +41,76 @@ def open():
             # 跳转页面
             windows = chrome.window_handles
             chrome.switch_to.window(windows[-1])
-            read_article = chrome.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div')
+
+            read_article = chrome.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[3]/div[2]/div[3]/div[2]/div[2]/div')
             read_article.click()
             # 跳转页面
             windows = chrome.window_handles
             chrome.switch_to.window(windows[-1])
             # 打开文章目录
-            open_list = chrome.find_element_by_xpath('//*[@id="d6df"]/div/div/div/div/div/section/div/div/div/div/div[1]/div/div/span')
+            open_list = chrome.find_element_by_xpath('//*[@id="25fa"]/div/div/div/div/div/div/div[1]/span/div')
             open_list.click()
             # 跳转页面
             windows = chrome.window_handles
             chrome.switch_to.window(windows[-1])
-            for i in range(7):
-                article = chrome.find_element_by_xpath('//*[@id="root"]/div/div/section/div/div/div/div/div/section/div/div/div/div[1]/div/section/div/div/div/div/div/section/div/div/div/div/div[3]/section/div/div/div/div/div/section/div/div/div[1]/div/div['+str(i+1)+']/div/div/div[1]/span')
+            time.sleep(3)
+            for i in range(1):
+                article = chrome.find_element_by_xpath('//*[@id="root"]/div/div/section/div/div/div/div/div/section/div/div/div/div/div/section/div/div/div/div/div/section/div/div/div/div/div[3]/section/div/div/div/div/div/section/div/div/div[1]/div/div['+str(i+1)+']/div/div/div[2]/span')
                 article.click()
                 time.sleep(3)
                 # 跳转页面
                 windows = chrome.window_handles
                 chrome.switch_to.window(windows[-1])
+                # 视听学习
+                listen_and_read = chrome.find_element_by_xpath('//*[@id="root"]/div/section/div/div/div/div/div[2]/section/div/div/div/div/div/div/div[3]/div[1]/div[1]/audio')
+                # listen_and_read.click()
+                listen_and_read.send_keys(Keys.SPACE)
                 chrome.execute_script("window.scrollTo(0,document.body.scrollHeight/2)")
-                time.sleep(3)
+                time.sleep(30)
                 chrome.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-                time.sleep(3)
-                chrome.close()
+                time.sleep(30)
+                # chrome.close()
                 windows = chrome.window_handles
-                chrome.switch_to.window(windows[-1])
-            time.sleep(10)
+                chrome.switch_to.window(windows[3])
+        #     视频
+            movie_1 = chrome.find_element_by_xpath('//*[@id="root"]/div/header/div[2]/div[1]/div[2]/a[2]')
+            movie_1.click()
+            time.sleep(3)
+            # 跳转页面
+            windows = chrome.window_handles
+            chrome.switch_to.window(windows[-1])
+            movie_2 = chrome.find_element_by_xpath('//*[@id="00c8"]/div/div/div/div/div/div/div[2]/img')
+            movie_2.click()
+            time.sleep(3)
+            # 跳转页面
+            windows = chrome.window_handles
+            chrome.switch_to.window(windows[-1])
+            for i in range(4):
+                for j in range(4):
+                    movie_3 = chrome.find_element_by_xpath('//*[@id="1novbsbi47k-5"]/div/div/div/div/div/div/section/div[3]/section/div/div/div[1]/div['+str(i+1)+']/div['+str(j+1)+']/section/div/div/div/div/div[1]/div/div/span/div')
+                    movie_3.click()
+                    time.sleep(3)
+                    # 跳转页面
+                    windows = chrome.window_handles
+                    chrome.switch_to.window(windows[-1])
+                    for temp in range(5,0,-1):
+                        height = 'window.scrollTo(0,document.body.scrollHeight/'+str(temp)+')'
+                        chrome.execute_script(height)
+                        time.sleep(3)
+                    windows = chrome.window_handles
+                    chrome.switch_to.window(windows[-2-j-i*4])
+            news = chrome.find_element_by_xpath('//*[@id="0454"]/div/div/div/div/div/div/div/div[1]/div/div[2]/div[5]/div/div')
+            news.click()
+            news_1 = chrome.find_element_by_xpath('//*[@id="17th9fq5c7l-5"]/div/div/div/div/div/div/section/div[3]/section/div/div/div[1]/div[1]/div[1]/section/div/div/div/div/div[1]/div/div/span/div')
+            news_1.click()
+            # 跳转页面
+            windows = chrome.window_handles
+            chrome.switch_to.window(windows[-1])
+            for temp in range(5, 3, -1):
+                height = 'window.scrollTo(0,document.body.scrollHeight/' + str(temp) + ')'
+                chrome.execute_script(height)
+                time.sleep(3)
+        time.sleep(60*3*6)
         chrome.quit()  # 退出
 
 if __name__ == '__main__':
